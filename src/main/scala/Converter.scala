c14d8bdbb3a2ca98176a155d6ab0eff87a2eec31
Rafal Kaczynski <rafalkaczynski02@gmail.com>
2022-03-28 11:26:48 +0200

    add error handling

--- src/main/scala/Converter.scala
+++ src/main/scala/Converter.scala
@@ -1,10 +1,21 @@
 import scala.io.StdIn.readLine
+import scala.sys.exit
 
 object Converter extends App {
 
   val numbers = Map('I' -> 1, 'V' -> 5, 'X' -> 10, 'L' -> 50, 'C' -> 100, 'D' -> 500, 'M' -> 1000)
 
-  def toArabic(s: String):Int = {
+  def isRoman(s: String): Boolean = {
+    for (c <- s) {
+      if (!numbers.keysIterator.contains(c)) {
+        println("Invalid number. Try again.")
+        return false
+      }
+    }
+    true
+  }
+
+  def toArabic(s: String): Int = {
     var sum = 0
 
     def sumRecursively(ls: List[Char]): Int = ls match {
@@ -15,15 +26,16 @@ object Converter extends App {
         else sum += numbers(h) + sumRecursively(t)
         sum
     }
-
     sumRecursively(s.toUpperCase.toList)
   }
 
   println("This is roman-to-arabic numeral converter type 'q' at any point exit")
-  var input = readLine("Type number in Roman notation: ")
+  var input = readLine("Roman notation number: ")
 
-  while(input.toLowerCase != "q") {
-    println(s"Arabic notation is: ${toArabic(input)}")
-    input = readLine("Type number in Roman notation: ")
+  while (input.toLowerCase != "q") {
+    if (isRoman(input)) {
+      println(s"Arabic notation is: ${toArabic(input)}")
+    }
+    input = readLine("Roman notation number: ")
   }
 }
